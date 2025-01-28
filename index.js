0f8141f7cd0da9bdc4dfe1724abc283e1fc4d080
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2025-01-28 16:42:29 +0100

    Update JavaScript event handler

---
+++ index.js
@@ -0,0 +1,13 @@
+function calculateLoan(){
+ const loanAmountValue = document.getElementById('loan-amount').value;
+
+ const interestRateValue = document.getElementById('interest-rate').value;
+
+ const monthsToPayValue = document.getElementById('months-to-pay').value;
+  
+ const interest = (loanAmountValue * (interestRateValue * 0.01))/monthsToPayValue;
+
+ const monthlyPayment = (loanAmountValue / monthsToPayValue + interest).toFixed(2);
+
+  document.getElementById('payment').innerHTML = `Monthly Payment: $${monthlyPayment}`;
+}
